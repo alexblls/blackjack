be659cb1631169f6b421171eb14f452df6d09f8f
María Lahuerta Magraner <maria.lahuerta.m@gmail.com>
2022-04-20 14:04:25 +0200

    ArregloFunciones

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 
@@ -13,12 +14,15 @@ public class Deck : MonoBehaviour
     public Button playAgainButton;
     public Text finalMessage;
     public Text probMessage;
+    public Text probMessage2;
+    public Text probMessage3;
 
     public int[] values = new int[52];
     int[] valuesRandom = new int[52];
 
-    int cardIndex = 0;    
-       
+    int cardIndex = 0;
+  
+
     private void Awake()
     {    
 
@@ -39,7 +43,7 @@ public class Deck : MonoBehaviour
          * En principio, la posición de cada valor se deberá corresponder con la posición de faces. 
          * Por ejemplo, si en faces[1] hay un 2 de corazones, en values[1] debería haber un 2.
          */
-        int valorCarta = 1;
+        int valorCarta = 11;
         int posicionCarta = 1;
 
         for (int i = 0; i < values.Length; i++)
@@ -48,13 +52,20 @@ public class Deck : MonoBehaviour
 
             if (posicionCarta % 13 == 0)
             {
-                valorCarta = 0;
+                valorCarta = 11;
             }
-
+            if (posicionCarta % 13 ==1)
+            {
+                valorCarta = 1;
+            }
             if (!(valorCarta >= 10))
             {
                 valorCarta++;
             }
+
+          
+
+           
         
             posicionCarta++;
         }
@@ -91,20 +102,26 @@ public class Deck : MonoBehaviour
             /*TODO:
              * Si alguno de los dos obtiene Blackjack, termina el juego y mostramos mensaje
              */
-            if (player.GetComponent<CardHand>().points == 21 || dealer.GetComponent<CardHand>().points == 21)
+            if (player.GetComponent<CardHand>().points == 21 )
             {
-                finalMessage.text = "Partida finalizada";
-                stickButton.interactable = false;
-                hitButton.interactable = false;
+                finalMessage.text = "Has ganado";
+                End();
+            }
+
+            if(dealer.GetComponent<CardHand>().points == 21)
+            {
+                finalMessage.text = "Has perdido";
+                End();
             }
         }
     }
 
     private void CalculateProbabilities()
     {
-        float PtosJugador = player.GetComponent<CardHand>().points;
-        float PtosDealer = dealer.GetComponent<CardHand>().points;
-        float cartasActuales = (values.Length - cardIndex);
+       // float PtosJugador = player.GetComponent<CardHand>().points;
+        //float PtosDealer = dealer.GetComponent<CardHand>().points;
+        float cartasActuales = 52- cardIndex;
+        int diferencia = player.GetComponent<CardHand>().points - dealer.GetComponent<CardHand>().points;
 
         /*TODO:
          * Calcular las probabilidades de:
@@ -115,40 +132,28 @@ public class Deck : MonoBehaviour
 
         float DealerMasJugador()
         {
-            float casosFavorables = 0;
-            Debug.Log("Resta"+ (PtosJugador - PtosDealer));
+            float casosFavorables = 0;          
 
-            if ((PtosJugador - PtosDealer) < 0){
-               
-                Debug.Log(PtosJugador);
-                Debug.Log(PtosDealer);
-                Debug.Log(cardIndex);
+            if (diferencia < 0){
 
                 return 1;
             }
-
+            else if (player.GetComponent<CardHand>().points == dealer.GetComponent<CardHand>().points)
+            {
+                return 0;
+            }
             else
             {
-                for(int i=0; i<cartasActuales; i++)
+                for (int i = cardIndex-1; i < valuesRandom.Length; i++)
                 {
-
-                    if (PtosDealer > PtosJugador)
+                    if (valuesRandom[i] >= diferencia)
                     {
                         casosFavorables++;
                     }
+                }               
 
-                }
-
-                Debug.Log(PtosJugador);
-                Debug.Log(PtosDealer);
-                Debug.Log(cardIndex);
-
-                return casosFavorables;
-
+                return casosFavorables/cartasActuales;
             }
-
-
-
         }
          
         
@@ -158,7 +163,7 @@ public class Deck : MonoBehaviour
            
 
             for(int i = 0; i < cartasActuales; i++){
-                float ActPtosJugador= PtosJugador + values[cardIndex-1];
+                float ActPtosJugador= player.GetComponent<CardHand>().points + values[cardIndex-1];
 
                 if ((ActPtosJugador > 17) && (ActPtosJugador < 21))
                 {
@@ -170,7 +175,7 @@ public class Deck : MonoBehaviour
 
         }
 
-        float Mas21()
+       /* float Mas21()
         {
             float casosFavorables = 0;
             for (int i = 0; i < cartasActuales; i++)
@@ -187,7 +192,7 @@ public class Deck : MonoBehaviour
 
 
 
-        }
+        }*/
 
 
         probMessage.text = DealerMasJugador().ToString();
@@ -212,25 +217,46 @@ public class Deck : MonoBehaviour
         player.GetComponent<CardHand>().Push(facesRandom[cardIndex], valuesRandom[cardIndex]/*,cardCopy*/);
         cardIndex++;
         CalculateProbabilities();
+        
+       
     }       
 
     public void Hit()
     {
+       
+      
         /*TODO: 
          * Si estamos en la mano inicial, debemos voltear la primera carta del dealer.
          */
-        
+
         //Repartimos carta al jugador
         PushPlayer();
 
+        float PtosJugador = player.GetComponent<CardHand>().points;
+
+
         /*TODO:
          * Comprobamos si el jugador ya ha perdido y mostramos mensaje
-         */      
+         */
+        if (PtosJugador > 21)
+        {
+            finalMessage.text = "Has perdido";
+            End();
+
+        }
+        if (PtosJugador == 21)
+        {
+            finalMessage.text = "Has ganado";
+
+            End();
+        }
 
     }
 
     public void Stand()
     {
+        //float PtosJugador = player.GetComponent<CardHand>().points;
+        //float PtosDealer = dealer.GetComponent<CardHand>().points;
         /*TODO: 
          * Si estamos en la mano inicial, debemos voltear la primera carta del dealer.
          */
@@ -239,10 +265,49 @@ public class Deck : MonoBehaviour
          * Repartimos cartas al dealer si tiene 16 puntos o menos
          * El dealer se planta al obtener 17 puntos o más
          * Mostramos el mensaje del que ha ganado
-         */                
-         
+         */
+        while (dealer.GetComponent<CardHand>().points <= 16 )
+        {
+            PushDealer();
+        }
+        if (dealer.GetComponent<CardHand>().points >= 17 || dealer.GetComponent<CardHand>().points <= 21)
+        {
+           
+            if (dealer.GetComponent<CardHand>().points == player.GetComponent<CardHand>().points)
+            {
+                finalMessage.text = "Empate";
+                End();
+                
+            }
+            if (player.GetComponent<CardHand>().points > dealer.GetComponent<CardHand>().points)
+            {
+                finalMessage.text = "Has ganado";
+                End();
+               
+            }
+            if (player.GetComponent<CardHand>().points < dealer.GetComponent<CardHand>().points)
+            {
+                finalMessage.text = "Has perdido";
+                End();
+               
+            }
+
+
+        }
+        if (dealer.GetComponent<CardHand>().points > 21)
+        {
+            finalMessage.text = "Has ganado";
+        }
+
     }
 
+    public void End()
+    {
+        dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
+        hitButton.interactable = false;
+        stickButton.interactable = false;
+
+    }
     public void PlayAgain()
     {
         hitButton.interactable = true;
@@ -253,6 +318,9 @@ public class Deck : MonoBehaviour
         cardIndex = 0;
         ShuffleCards();
         StartGame();
+        
+
+
     }
     
 }
